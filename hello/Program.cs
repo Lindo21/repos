013b4447ed4a03392ad0b7e894f79fe2b602e5e2
Lindo21 <Lindon.Ngonga@zamstats.gov.zm>
2019-12-20 16:33:41 +0200

    Removed middle name

---
+++ hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Lindon Ngonga";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My full name is {0}", myname);
+        }
+    }
+}
